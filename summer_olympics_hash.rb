a678a6a7e5e5f1281bfa600c9c1f0d2baa0154a6
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-11 20:57:46 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -34,7 +34,7 @@ olympics_hash.each do |place,year|
   puts "The #{year} summer olympics took place in #{place}."
 end
 
-puts olympics_hash.keys.uppercased
+puts olympics_hash
 
 
 # def iterate_through_keys
