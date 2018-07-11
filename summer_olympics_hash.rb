dde1abd8799fb899a0a3aea5e57fc7597f21feac
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-11 20:44:34 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -3,11 +3,22 @@ place= ["sydney", "athens", "Beijing", "London", "Atlanta"]
 year= [ "2000", "2004", "2008", "2012", "1996"]
 
 
-olympics_hash= {}
-olympics_hash["Sydney"] = "2000"
-olympics_hash["Athens"]= "2004"
-olympics_hash["Beijing"]= "2008"
-olympics_hash["London"]= "2012"
+# olympics_hash= {}
+# olympics_hash["Sydney"] = "2000"
+# olympics_hash["Athens"]= "2004"
+# olympics_hash["Beijing"]= "2008"
+# olympics_hash["London"]= "2012"
+# puts olympics_hash
+
+olympics_hash = {}
+
+count = 0
+
+olympics_hash.each do |place|
+  olympics_hash[place] = year[count]
+  count += 1
+end
+
 puts olympics_hash
 
 new_olympic_city = "Atlanta"
